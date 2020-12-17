06cc91fa8417e12674791e9cd111eebfabc9e3f3
koran23 <ahdari33@gmail.com>
2020-12-17 12:20:50 -0700

    signup

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -3,7 +3,7 @@ import thunk from 'redux-thunk';
 import sessionReducer from './session';
 
 const rootReducer = combineReducers({
-  user: sessionReducer
+    session: sessionReducer 
 });
 
 let enhancer;
